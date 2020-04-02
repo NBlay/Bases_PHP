4414847b576a61e9b9fba23d4fc147aa93f8d08e
NBlay <62250298+NBlay@users.noreply.github.com>
2020-04-02 12:36:36 +0200

    VraiouFaux.php

---
+++ VraiouFaux.php
@@ -0,0 +1,12 @@
+<?php
+echo "Dans cette suite; quels nombres sont pairs? \n";
+$OCCURENCE=array('16','7','15');
+foreach ($OCCURENCE as $value)
+{
+    if ($value%2==0)
+      echo "vrai";
+     else
+      echo "faux";
+}
+
+ ?>
